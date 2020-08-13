16bcc212a02db8ae04adc402a471380976e188c5
Harsh Jha <HARSHJHASD@users.noreply.github.com>
2020-08-13 17:48:22 +0530

    banner changed

---
+++ app.js
@@ -0,0 +1,46 @@
+//jshint esversion:6
+const express = require("express");
+const app = express();
+
+
+
+// all get requests
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+app.get("/index.html",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+app.get("/gallery.html",function(req,res){
+  res.sendFile(__dirname+"/gallery.html");
+});
+app.get("/about.html",function(req,res){
+  res.sendFile(__dirname+"/about.html");
+});
+app.get("/blog.html",function(req,res){
+  res.sendFile(__dirname+"/blog.html");
+});
+app.get("/contact.html",function(req,res){
+  res.sendFile(__dirname+"/contact.html");
+});
+app.get("/pricing.html",function(req,res){
+  res.sendFile(__dirname+"/pricing.html");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//TODO
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
